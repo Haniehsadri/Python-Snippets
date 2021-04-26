de207f2c3e9ae3c05dceab689b86a6092baf23f6
Hanieh Sadri <haniehsadri77@gmail.com>
2021-04-26 10:36:50 +0430

    added the initial repository

---
+++ main.py
@@ -0,0 +1,34 @@
+print ("Hello world")
+print ("   /|")
+print ("  / |")
+print (" /__|")
+print ("There once was a man named john  ")
+print("he was 60  ")
+print ("he liked his name john ")
+print ("but he didnt like being 60")
+
+charactername="john "
+characterage=60.5
+print("There once was a man named  "+charactername)
+print("he was" + str(characterage) + "years")
+print("he liked his name  "+charactername)
+print ("but he didnt like being "+str(characterage))
+phrase="Girrafe academy "
+print (phrase+"is cool")
+print(phrase.lower())
+print(phrase.upper().isupper())
+print (len(phrase))
+print(phrase[0].isupper())
+print(phrase[1].isupper())
+print(phrase[7])
+print (phrase.replace("Girrafe", "elephant"))
+number=-5
+print(abs(number))
+print(pow(number,2))
+print((max(2,3,6,7
+            )))
+name=input("enter your name :")
+print ("your name is :" +name)
+
+
+
